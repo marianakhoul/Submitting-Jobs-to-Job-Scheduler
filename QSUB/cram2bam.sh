c0e29cd8c9aeeeb2f724d0e909834e0f0a1ca2d5
Maria Nakhoul <31465978+marianakhoul@users.noreply.github.com>
2022-04-22 15:47:15 -0400

    Update cram2bam.sh

--- QSUB/cram2bam.sh
+++ QSUB/cram2bam.sh
@@ -13,8 +13,8 @@ for i in `cat ~/cram_file_paths.txt`;
 	echo 'source /etc/profile' >> $HOME/cram2bam_jobs/${base_name}.qsub
 	echo '#$ -S /bin/bash' >> $HOME/cram2bam_jobs/${base_name}.qsub 
 	echo '#$ -pe pvm 32'"\n\n" >> $HOME/cram2bam_jobs/${base_name}.qsub
-	echo 'module load /mnt/storage/spack/share/spack/modules/linux-ubuntu16.04-x86_64/samtools-1.9-gcc-5.4.0-jjq5nua'"\n" >> $HOME/cram2bam_jobs/${base_name}.qsub
-	echo "samtools view -T /mnt/storage/labs/sviswanathan/GATK_ref/Homo_sapiens_assembly38.fasta -b -o ${i}.bam ${i}.cram" >> $HOME/cram2bam_jobs/${base_name}.qsub
+	echo 'module load /modules/linux-ubuntu16.04-x86_64/samtools-1.9-gcc-5.4.0-jjq5nua'"\n" >> $HOME/cram2bam_jobs/${base_name}.qsub
+	echo "samtools view -T /path/to/reference/fasta/Homo_sapiens_assembly38.fasta -b -o ${i}.bam ${i}.cram" >> $HOME/cram2bam_jobs/${base_name}.qsub
 	done
 
 for i in `ls $HOME/cram2bam_jobs`;
